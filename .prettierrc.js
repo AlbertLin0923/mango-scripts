db982873fd19fbfe4f8273a00a2238194c31d480
linsf <linsf@akulaku.com>
2022-06-17 11:08:13 +0800

    feat: 修改prettier配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,8 @@
 module.exports = {
   semi: false,
-  tabWidth: 2,
   singleQuote: true,
-  printWidth: 80,
   trailingComma: 'none',
+  printWidth: 100,
   proseWrap: 'never',
   endOfLine: 'auto',
   overrides: [
