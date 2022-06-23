7417b835f951d687e71d3d0454acae6a069f33df
linsf <linsf@akulaku.com>
2022-06-23 14:06:11 +0800

    feat: 完善@mango-scripts/esp-config

--- packages/esp-config/src/stylelint.ts
+++ packages/esp-config/src/stylelint.ts
@@ -3,19 +3,20 @@ export default {
     'stylelint-config-standard',
     'stylelint-config-standard-scss',
     'stylelint-config-css-modules',
+    'stylelint-config-rational-order',
     'stylelint-config-prettier'
   ],
-  plugins: ['stylelint-declaration-block-no-ignored-properties'],
-  rules: {
-    'no-descending-specificity': null,
-    'function-url-quotes': 'always',
-    'selector-attribute-quotes': 'always',
-    'font-family-no-missing-generic-family-keyword': null, // iconfont
-    'plugin/declaration-block-no-ignored-properties': true,
-    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
-    // webcomponent
-    'selector-type-no-unknown': null,
-    'value-keyword-case': ['lower', { ignoreProperties: ['composes'] }]
-  },
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts']
+  plugins: [],
+  rules: {},
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  overrides: [
+    {
+      files: ['**/*.less'],
+      customSyntax: 'postcss-less'
+    },
+    {
+      files: ['**/*.styl', '**/*.stylus'],
+      customSyntax: 'postcss-styl'
+    }
+  ]
 }
